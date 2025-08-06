847e73d39466910d9ec94a8ed153743d5420999a
johnanvesh14 <johnanvesh.akkarapaka@gmail.com>
2025-08-06 13:29:37 +0530

    ABout created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,10 @@ namespace Employewebapp.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
